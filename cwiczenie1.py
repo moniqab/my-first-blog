c213b4bf967342ee289e34f819fca41955c88ef9
Moniqa <monika.buchaniewicz@gmail.com>
2019-09-07 16:59:26 +0200

    My Django Girls app, first commit

---
+++ cwiczenie1.py
@@ -0,0 +1,11 @@
+def hi(imie):
+    print('Hej ' + imie + '!')
+
+dziewczyny = ['Rachel', "Monika", 'Ola', "Maja"]
+for imie in dziewczyny:
+    hi(imie)
+    print('Kolejna dziewczyna')
+for i in range(1, 6):
+    print(i) 
+
+
